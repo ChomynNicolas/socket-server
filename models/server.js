4550bf8c43b58b674dae85a63aa057d5a1874809
Nicolas Chomyn <chomynnicolas2@gmail.com>
2024-07-05 10:28:00 -0400

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,53 @@
+//Servidor de express
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require('path');
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    //http server
+    this.server = http.createServer(this.app);
+
+    //Configuracion del socket server
+    this.io = socketio(this.server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+    });
+  }
+
+  middlewares(){
+    //desplegar el directorio publico
+    this.app.use(express.static(path.resolve(__dirname,'../public')));
+
+  }
+
+  configurarSockets(){
+    //hacer algo
+    new Sockets(this.io);
+  }
+
+  execute(){
+    //Inicializar Middelwares
+    this.middlewares();
+
+    //Inicializar Sockets
+    this.configurarSockets();
+
+    //Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en el puerto :", this.port);
+    });
+    
+  }
+
+
+
+}
+
+module.exports = Server;
